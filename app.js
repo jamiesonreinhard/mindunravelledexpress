be071735ffb44b374dcf31f2584f8c0e0c880b62
jamiesonreinhard <59634117+jamiesonreinhard@users.noreply.github.com>
2020-04-29 18:32:21 -0500

    Added login, register, and admin pages. Required necessary modules. Added get requests.

--- app.js
+++ app.js
@@ -3,19 +3,52 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const _ = require("lodash");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+
 
 const app = express();
 
-app.set("view engine", "ejs");
-
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {
+app.use(session({
+  secret: "This is my super duper secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect('mongodb://localhost:27017/userDB', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
+
+mongoose.set("useCreateIndex", true);
+
+//THIS IS THE USER SCHEMA
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 //THIS IS THE SCHEMA FOR THE "REGULAR" VIDEO POST
 const postsSchema = new mongoose.Schema({
@@ -52,6 +85,58 @@ const featuredblogsSchema = new mongoose.Schema({
 });
 
 const Featuredblog = mongoose.model("Featuredblog", featuredblogsSchema);
+
+//THESE ARE THE admin site routes
+
+app.get("/login", function(req, res){
+  res.render("login");
+})
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.get("/admin", function(req, res){
+  res.render("admin");
+});
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+})
+
+app.post("/register", function(req, res) {
+
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local") (req, res, function(){
+        res.redirect("/admin");
+      })
+    }
+  })
+
+});
+
+app.post("/login", function(req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+
+  });
+
+  req.login(user, function(err){
+    if(err){
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/admin");
+      })
+    }
+  })
+  });
 
 ///////////root route
 app.get("/", function (req, res) {
@@ -140,10 +225,9 @@ app.post("/deleteFeaturedVid", function (req, res) {
     } else {
       res.redirect("/featuredvideo");
 
-})
+}
 
-    }
-  });
+    });
 });
 
 //////////BLOG POST SECTION
